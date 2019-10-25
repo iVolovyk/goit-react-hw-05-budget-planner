c54aa1819770324eb8aa853f35a9c5c63941750f
Ihor Volovyk <ihor.volovyk@gmail.com>
2019-10-26 01:24:12 +0300

    Updates

---
+++ precache-manifest.d08878bc52fa7ec9e2182b3223e8556f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2f6c909d4cdebb566de55c17e09da4",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "6b3f92ad8a75457dc325",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "1a88f9580cb2f83d59d9",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.fd2708fa.chunk.js"
+  },
+  {
+    "revision": "6b3f92ad8a75457dc325",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.387aaab6.chunk.js"
+  },
+  {
+    "revision": "dffe8cd1e2fc699743fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.d2e8316a.js"
+  }
+]);
